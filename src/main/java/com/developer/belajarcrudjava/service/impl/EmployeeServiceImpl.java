06867488c243348f02bc8cbe2115c03f40a5fb9e
engkoskostaman97 <engkoskostaman759@gmail.com>
2023-05-25 14:23:19 +0700

    simple crud use lombok javaspringboot

--- src/main/java/com/developer/belajarcrudjava/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/developer/belajarcrudjava/service/impl/EmployeeServiceImpl.java
@@ -42,3 +42,17 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     }
 }
+//    Penjelasan kode diatas yaitu :
+//        - @Service : adalah annotasi bahwa kita membuat komponen untuk layer Service
+//        - @Autowired adalah annotasi yang menginjika kita untuk menginject sebuah class dalam hal
+//        ini yaitu kita menginject interface ProductRepository
+//        - List<Product> findAllProducts() method yang digunakan untuk mengembalikan
+//        nilai product yang ada di database yang nantinya akan dikembalikan ke controller sebagai
+//        pemanggil dari method ini
+//        - Product findProductById(Long id) method untuk mencari data single Product
+//        berdasarkan id yang diberikan dari Controller. Namun dalam method ini diberi logic jika data
+//        product nya tidak ada di database berdasarkan ID yang diberikan maka method ini
+//        mengembalikan object Product kosong.
+//        - Product saveorUpdateProduct(Product product) method untuk menyimpan data
+//        produk baru atau memperbaharui data product.
+//        - deleteProduct(Long id) method ini untuk menghapus data produk berdasarkan ID
